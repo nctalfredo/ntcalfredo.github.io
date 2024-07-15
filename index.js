2ed1253e136ec97540bfb24178687d018cf82c29
nctalfredo <169058065+nctalfredo@users.noreply.github.com>
2024-07-15 12:12:20 -0500

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function comprarCurso(nombreCurso) {
-    alert("Has comprado el " + nombreCurso);
-}
-
-document.getElementById("formularioContacto").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Mensaje enviado. Nos pondremos en contacto contigo pronto.");
-});
